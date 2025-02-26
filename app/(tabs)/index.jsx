8f81156e9e61165be6f70cfecd677ec39997929d
Praveen Raju <praveenraju2889@gmail.com>
2025-02-26 11:37:16 +0400

    React Task

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,195 @@
+import { View, FlatList, Text, StyleSheet, ActivityIndicator,ImageBackground, Button, Image } from 'react-native'
+import imgMIB from "@/assets/images/Work.png"
+//import font from "@/assets/fonts/SpaceMono-Regular.ttf"
+
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+import React , {useEffect, useState,useCallback} from 'react'
+
+// const [data, setData] = useState([]);
+//   const [page, setPage] = useState(1);
+//   const [loading, setLoading] = useState(false);
+// const DATA = [
+//   {
+//     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//     title: 'First Item',
+//   },
+//   {
+//     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//     title: 'Second Item',
+//   },
+//   {
+//     id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//     title: 'Third Item',
+//   },
+//   {
+//     id: '58694a0f-3da1-471f-bd96-145571e29d71',
+//     title: 'Fourth Item',
+//   },
+//   {
+//     id: '58694a0f-3da1-471f-bd96-145571e29d712',
+//     title: 'Fifth Item',
+//   },
+// ];
+
+// const Item = ({ title }) => (
+//   <View style={styles.item}>
+//     <Image source={imgMIB}
+//       style={styles.image1} />
+//     <Text style={styles.title}>{title}</Text>
+//   </View>
+// );
+const renderItem = ({ item }) => (
+  <View style={styles.item}>
+    
+    <Image source={ {uri:item.thumbnail} } style={styles.image1} />
+    <Text style={styles.title}>{item.title}</Text>
+    <Text style={styles.description}>{item.description}</Text>
+  </View>
+);
+
+
+const app = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  //const [loading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const getMovies = async () => {
+    try {
+      //const response = await fetch('https://reactnative.dev/movies.json');
+      //const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+      const response = await fetch('https://dummyjson.com/products');
+      
+      const json = await response.json();
+      setData(json.products);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchData = useCallback(async () => {
+    if (isLoading) return;
+    setLoading(true);
+    
+    try {
+      const newData = Array.from({ length: 20 }, (_, i) => ({
+        id: i + 35 + (page - 1) * 20,
+        title: `Item ${i + 1 + (page - 1) * 20}`,
+        thumbnail: 'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png'
+      }));
+      setData(prevData => [...prevData, ...newData]);
+      setPage(prevPage => prevPage + 1);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  }, [isLoading, page]);
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <View style={styles.container}>
+     
+        <Text style={styles.text}>MIB 2.0</Text>
+        {/* <Button
+          title="Learn More"
+          color="#841584"
+          backgroundColor="#ffffff"
+          accessibilityLabel="Learn more about this purple button"
+        /> */}
+        <SafeAreaProvider style={styles.containerBG}>
+          <SafeAreaView style={styles.containerBG}>
+            <FlatList
+              data={data}
+              //renderItem={({item}) => <Item title={item.title} />}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+              onEndReached={fetchData}
+              //onEndReachedThreshold={0.5}
+            />
+          </SafeAreaView>
+        </SafeAreaProvider>
+         {/* <ImageBackground
+        source={imgMIB}
+        resizeMode="cover"
+        style={styles.image}>
+      </ImageBackground> */}
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    borderBlockColor: 'red',
+    height:"80%"
+    //marginVertical: 70,
+  },
+  containerBG:{
+    flex: 1,
+   //
+    //marginVertical: 70,
+   //marginBlockStart:70,
+    width: '100%',
+    height: '80%',
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    backgroundColor:'rgba(0,0,0,0.5)'
+
+  },
+  text: {
+    //marginTop:70,
+    //marginVertical: 30,
+    marginBlockStart:20,
+    marginHorizontal: 30,
+    color: 'rgba(243, 72, 103, 0.47)',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    //backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+
+  item: {
+    backgroundColor: '#a9abaa',
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    //flexDirection: 'row'
+  },
+  title: {
+    fontFamily:"SpaceMono-Regular",
+    //fontStyle:'normal',
+    fontSize: 25,
+    marginVertical: 0,
+    //marginHorizontal: 16,
+    textAlign: 'left',
+
+  },
+  subtitle: {
+    fontFamily:"SpaceMono-Regular",
+    //fontStyle:'normal',
+    fontSize: 10,
+    marginVertical: 0,
+    //marginHorizontal: 16,
+    textAlign: 'left',
+
+  },
+  image1: {
+    width: 200,
+    height: 200,
+    borderRadius: 5,
+  }
+
+})
